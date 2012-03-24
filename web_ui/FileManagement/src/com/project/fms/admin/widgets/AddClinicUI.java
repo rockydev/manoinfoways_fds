309a6d0f111e8e27dc42662bababa9e07148ee16
deadlydeo <deepakmdeo@gmail.com>
2012-03-25 01:15:41 +0530

    End of 1st UI Goal - Finished Screens for Add Clinic. Implemented FormValuesManager

--- web_ui/FileManagement/src/com/project/fms/admin/widgets/AddClinicUI.java
+++ web_ui/FileManagement/src/com/project/fms/admin/widgets/AddClinicUI.java
@@ -1,5 +1,6 @@
 package com.project.fms.admin.widgets;
 
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
@@ -9,14 +10,15 @@ import com.smartgwt.client.widgets.tab.Tab;
 import com.smartgwt.client.widgets.tab.TabSet;
 
 public class AddClinicUI extends VLayout{
+
 	
 	public AddClinicUI(){
 		
 		setMargin(10);
 		setMembersMargin(10);
-		ValuesManager formValuesManager = new ValuesManager();
+		final ValuesManager formValuesManager = new ValuesManager();
 		
-		TabSet clinicTabSet = new TabSet();
+		final TabSet clinicTabSet = new TabSet();
 		clinicTabSet.setWidth100();
 		clinicTabSet.setHeight("80%");
 		
@@ -27,19 +29,22 @@ public class AddClinicUI extends VLayout{
 		
 		
 		Tab clinicConnectionTab = new Tab("Clinic Connection");
-		ClinicConnectionDetailsForm clinicConnectionFormWidget = new ClinicConnectionDetailsForm();
+		final ClinicConnectionDetailsForm clinicConnectionFormWidget = new ClinicConnectionDetailsForm();
 		clinicConnectionFormWidget.setValuesManager(formValuesManager);
 		clinicConnectionTab.setPane(clinicConnectionFormWidget);
 		
 		Tab clinicManagersTab = new Tab("Clinic Managers Information");
-		ClinicManagersDataForm clinicManagersWidget = new ClinicManagersDataForm();
+		final ClinicManagersDataForm clinicManagersWidget = new ClinicManagersDataForm();
 		clinicManagersWidget.setValuesManager(formValuesManager);
 		clinicManagersTab.setPane(clinicManagersWidget);
 		
+		Tab clinicRequirementsTab = new Tab("Clinic Requirements Information");
 		
-		clinicTabSet.addTab(clinicDataTab);
-		clinicTabSet.addTab(clinicConnectionTab);
-		clinicTabSet.addTab(clinicManagersTab);
+		ClinicRequirementsForm clinicRequirementsWidget = new ClinicRequirementsForm();
+//		clinicRequirementsWidget.setValuesManager(formValuesManager);
+		clinicRequirementsTab.setPane(clinicRequirementsWidget);
+		
+		clinicTabSet.setTabs(clinicDataTab, clinicConnectionTab, clinicManagersTab, clinicRequirementsTab);
 		
 		
 		addMember(clinicTabSet);
@@ -49,7 +54,22 @@ public class AddClinicUI extends VLayout{
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
+				
+				//Comment the below steps to skip Validations 
+				formValuesManager.validate();
+				if(clinicDataFormWidget.hasErrors()){
+					SC.say("Errors in tab clinicDataFormWidget");
+					clinicTabSet.selectTab(0);
+				}else if(clinicConnectionFormWidget.hasErrors()){
+					SC.say("Errors in tab clinicConnectionFormWidget");
+					clinicTabSet.selectTab(1);
+				}else if(clinicManagersWidget.hasErrors()){
+					SC.say("Errors in tab clinicManagersWidget");
+					clinicTabSet.selectTab(2);
+				}
+				//Add your DS logic here
+				else{
+				
 //				clinicDataFormWidget.clinicDataDs.addData(new ClinicData(
 //					((TextItem) clinicDataFormWidget.getItem("clinicAbbr")).getValueAsString().toUpperCase(),
 //					((TextItem) clinicDataFormWidget.getItem("clinicName")).getValueAsString(),
@@ -58,6 +78,7 @@ public class AddClinicUI extends VLayout{
 //					((TextItem) clinicDataFormWidget.getItem("location")).getValueAsString(),
 //					((TextItem) clinicDataFormWidget.getItem("country")).getValueAsString(),
 //					((TextItem) clinicDataFormWidget.getItem("zipcode")).getValueAsString()));
+				}
 			}
 		});
 		addMember(submitButton);
